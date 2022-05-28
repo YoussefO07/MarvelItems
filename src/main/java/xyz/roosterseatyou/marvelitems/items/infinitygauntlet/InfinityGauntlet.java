b792c44173dfc34b1bc18280b53913cedfa50920
Roosterseatyou <ltbkthree@icloud.com>
2022-05-28 12:02:38 -0500

    Added stuff that was needed...

--- src/main/java/xyz/roosterseatyou/marvelitems/items/infinitygauntlet/InfinityGauntlet.java
+++ src/main/java/xyz/roosterseatyou/marvelitems/items/infinitygauntlet/InfinityGauntlet.java
@@ -1,30 +1,43 @@
 package xyz.roosterseatyou.marvelitems.items.infinitygauntlet;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class InfinityGauntlet {
     public static final Component NAME = Component.text("Infinity Gauntlet");
-
-
+    public static ItemStack INF_GAUNTLET;
 
     public static ItemStack init() {
         getItem();
         return null;
     }
 
-    public static ItemStack getItem() {
+    public static void getItem() {
         ItemStack item = new ItemStack(Material.GOLDEN_CHESTPLATE);
         ItemMeta meta = item.getItemMeta();
         List<Component> lore = new ArrayList<>();
         meta.displayName(NAME);
         lore.add(Component.text("Perfectly balanced, as all things should be."));
+        lore.add(Component.text(UUID.randomUUID().toString()).color(TextColor.color(46, 44, 44)));
+        meta.lore(lore);
         item.setItemMeta(meta);
-        return item;
+        INF_GAUNTLET = item;
+    }
+
+    public static ItemStack genItem() {
+        ItemStack it = INF_GAUNTLET.clone();
+        ItemMeta meta = it.getItemMeta();
+        List<Component> lore = meta.lore();
+        lore.set(1, Component.text(UUID.randomUUID().toString()).color(TextColor.color(46, 44, 44)));
+        meta.lore(lore);
+        it.setItemMeta(meta);
+        return it;
     }
 }
